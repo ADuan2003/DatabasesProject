38003b10c9c13a361be1acd8b0df1def68c6295f
Anthony Duan <123067489+ADuan2003@users.noreply.github.com>
2024-11-17 20:12:47 -0500

    Update create.sql

--- create.sql
+++ create.sql
@@ -31,3 +31,45 @@ CREATE TABLE EmployeeInfo
   FOREIGN KEY K3 (Coverage) references HealthInsurance(coverage) ON DELETE CASCADE,
   PRIMARY KEY (EmployeeID));
 #how to verify that salary is within range?
+
+CREATE TABLE Departments
+(department CHAR(20) NOT NULL,
+  PRIMARY KEY (department));
+
+CREATE TABLE EmployeeAssignments
+(EmployeeID SMALLINT NOT NULL,
+  Department CHAR(20) NOT NULL,
+  StartDate DATE NOT NULL,
+  EndDate DATE NOT NULL,
+  FOREIGN KEY K4 (EmployeeID) references EmployeeInfo(EmployeeID) ON DELETE CASCADE,
+  FOREIGN KEY K5 (Department) references Departments(department) ON DELETE CASCADE,
+  PRIMARY KEY (EmployeeID, Department, StartDate));
+
+CREATE TABLE Benefits
+(benefit CHAR(20) NOT NULL,
+  PRIMARY KEY (benefit));
+
+CREATE TABLE EmployeeBenefits 
+(EmployeeID SMALLINT NOT NULL,
+  Benefit CHAR(20) NOT NULL,
+  StartDate DATE NOT NULL,
+  EndDate DATE NOT NULL,
+  FOREIGN KEY K6 (EmployeeID) references EmployeeInfo(EmployeeID) ON DELETE CASCADE,
+  FOREIGN KEY K7 (Benefit) references Benefits(benefit) ON DELETE CASCADE,
+  PRIMARY KEY (EmployeeID, Department, StartDate));
+
+CREATE TABLE ProjectStatus
+(status CHAR(20) NOT NULL,
+  PRIMARY KEY (status));
+
+CREATE TABLE Projects
+(Project CHAR(20) NOT NULL,
+  Department CHAR(20) NOT NULL,
+  ProjectLeader SMALLINT NOT NULL,
+  Status CHAR(20) NOT NULL,
+  FOREIGN KEY K8 (Department) references Department(department) ON DELETE CASCADE,
+  FOREIGN KEY K9 (ProjectLeader) references EmployeeInfo(EmployeeID) ON DELETE CASCADE,
+  FOREIGN KEY KA (Status) references ProjectStatus(status) ON DELETE CASCADE,
+  PRIMARY KEY (Project));
+
+
