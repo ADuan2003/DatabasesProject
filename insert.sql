e50611430da99dc4621974f75ccea81098fc7ebc
Anthony Duan <123067489+ADuan2003@users.noreply.github.com>
2024-11-17 21:12:35 -0500

    Create insert.sql
    Skeleton of insertions (these are insertions we'll need)

---
+++ insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO HealthInsurance(coverage)
+VALUES
+('research scientist'),
+('applied scientist'),
+('engineer/programmer'),
+('senior programmer'),
+('director');
+
+INSERT INTO Benefits(benefit)
+VALUES
+('health'),
+('dental'),
+('life insurance'),
+('short/long term disability');
+
+INSERT INTO ProjectStatus(status)
+VALUES
+('new'),
+('in-progress'),
+('completed'),
+('on-hold');
+
+#these are the specified values to insert; I suppose we'll write up functions in whatever language we use that enables more detailed insertions
