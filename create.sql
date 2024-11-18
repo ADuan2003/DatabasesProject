84002812015af4bb3ea7b77c4fc599ebc3800fbb
Anthony Duan <123067489+ADuan2003@users.noreply.github.com>
2024-11-18 02:25:32 -0500

    Update create.sql
    Thought these CHAR(length) values worked better

--- create.sql
+++ create.sql
@@ -1,6 +1,6 @@
 #a list of commands for creating tables
 CREATE TABLE Positions 
-(position CHAR(20) NOT NULL,
+(position CHAR(40) NOT NULL,
   minSalary SMALLINT NOT NULL,
   maxSalary SMALLINT NOT NULL,
   PRIMARY KEY (position),
@@ -8,24 +8,24 @@ CREATE TABLE Positions
   CHECK (minSalary <= maxSalary));
 
 CREATE TABLE HealthInsurance
-(coverage CHAR(20) NOT NULL,
+(coverage CHAR(40) NOT NULL,
   PRIMARY KEY (coverage));
 
 CREATE TABLE EmployeeInfo
 (EmployeeID SMALLINT NOT NULL,
   SSN SMALLINT NOT NULL,
-  Name CHAR(20) NOT NULL,
+  Name CHAR(30) NOT NULL,
   gender CHAR(20) NOT NULL,
   DoB DATE NOT NULL,
-  PrimaryAddress CHAR(80) NOT NULL,
+  PrimaryAddress CHAR(100) NOT NULL,
   PhoneNumber CHAR(20) NOT NULL,
   HighestDegree CHAR(20) NOT NULL,
   YearsExperience SMALLINT NOT NULL,
-  HiringPosition CHAR(20) NOT NULL,
+  HiringPosition CHAR(40) NOT NULL,
   HiringSalary SMALLINT NOT NULL,
-  CurrentPosition CHAR(20) NOT NULL,
+  CurrentPosition CHAR(40) NOT NULL,
   CurrentSalary SMALLINT NOT NULL,
-  Coverage CHAR(20) NOT NULL,
+  Coverage CHAR(40) NOT NULL,
   FOREIGN KEY K1 (HiringPosition) references Positions(position) ON DELETE CASCADE,
   FOREIGN KEY K2 (CurrentPosition) references Positions(position) ON DELETE CASCADE,
   FOREIGN KEY K3 (Coverage) references HealthInsurance(coverage) ON DELETE CASCADE,
@@ -33,12 +33,12 @@ CREATE TABLE EmployeeInfo
 #how to verify that salary is within range?
 
 CREATE TABLE Departments
-(department CHAR(20) NOT NULL,
+(department CHAR(30) NOT NULL,
   PRIMARY KEY (department));
 
 CREATE TABLE EmployeeAssignments
 (EmployeeID SMALLINT NOT NULL,
-  Department CHAR(20) NOT NULL,
+  Department CHAR(30) NOT NULL,
   StartDate DATE NOT NULL,
   EndDate DATE NOT NULL,
   FOREIGN KEY K4 (EmployeeID) references EmployeeInfo(EmployeeID) ON DELETE CASCADE,
@@ -46,12 +46,12 @@ CREATE TABLE EmployeeAssignments
   PRIMARY KEY (EmployeeID, Department, StartDate));
 
 CREATE TABLE Benefits
-(benefit CHAR(20) NOT NULL,
+(benefit CHAR(30) NOT NULL,
   PRIMARY KEY (benefit));
 
 CREATE TABLE EmployeeBenefits 
 (EmployeeID SMALLINT NOT NULL,
-  Benefit CHAR(20) NOT NULL,
+  Benefit CHAR(30) NOT NULL,
   StartDate DATE NOT NULL,
   EndDate DATE NOT NULL,
   FOREIGN KEY K6 (EmployeeID) references EmployeeInfo(EmployeeID) ON DELETE CASCADE,
@@ -63,8 +63,8 @@ CREATE TABLE ProjectStatus
   PRIMARY KEY (status));
 
 CREATE TABLE Projects
-(Project CHAR(20) NOT NULL,
-  Department CHAR(20) NOT NULL,
+(Project CHAR(40) NOT NULL,
+  Department CHAR(30) NOT NULL,
   ProjectLeader SMALLINT NOT NULL,
   Status CHAR(20) NOT NULL,
   FOREIGN KEY K8 (Department) references Department(department) ON DELETE CASCADE,
@@ -73,13 +73,13 @@ CREATE TABLE Projects
   PRIMARY KEY (Project));
 
 CREATE TABLE Roles
-(role CHAR(20) NOT NULL,
+(role CHAR(30) NOT NULL,
   PRIMARY KEY (role));
 
 CREATE TABLE EmployeeProjects
 (EmployeeID SMALLINT NOT NULL,
-  Project CHAR(20) NOT NULL,
-  Role CHAR(20) NOT NULL,
+  Project CHAR(40) NOT NULL,
+  Role CHAR(30) NOT NULL,
   Current BOOLEAN NOT NULL,
   FOREIGN KEY KB (EmployeeID) references EmployeeInfo(EmployeeID) ON DELETE CASCADE,
   FOREIGN KEY KC (Project) references Projects(Project) ON DELETE CASCADE,
