ceeb6bc4f7758f141955086c14664244078ec4fa
Pegistar5 <ec1253@georgetown.edu>
2024-11-28 14:24:42 -0500

    Attempting to set up form submission

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 from database import * 
 
 app = Flask(__name__)
@@ -8,6 +8,9 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/query/")
+@app.route("/query/", methods =['GET', 'POST'])
 def queryPage():
-    return render_template("query.html")
+    table = None
+    if request.method == ['POST']:
+        table = request.form.get('show') #for some reason this isn't getting set... 
+    return render_template("query.html", query = table)
