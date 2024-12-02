b0cce9e0e88134671ec28657916b1fd853d4056a
Pegistar5 <ec1253@georgetown.edu>
2024-12-02 16:38:16 -0500

    Swapping certain render_templates to redirect(url_for)

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
@@ -10,17 +10,16 @@ def home():
 
 @app.route("/add/", methods=["POST"])
 def add():
-    operation = None
     try:
         if request.form.get("addRow") == "Health Insurance":
-            return render_template("addHealthInsurance.html")
+            return redirect(url_for("insertIntoHealthInsurance"))
         elif request.form.get("addRow") == "BenefitType":
-            return render_template("home.html")
+            return render_template("add.html")
     except:
         return render_template("error.html")
-    return render_template("home.html")
+    return render_template("add.html")
 
-@app.route("/update/", methods=["POST"])
+@app.route("/update/", methods=["GET", "POST"])
 def update():
     return render_template("query.html", query = "update") #placeholder
 
@@ -42,8 +41,8 @@ def insertIntoHealthInsurance():
     if request.method == 'POST':
         try:
             coverage = request.form.get('insertHI') #request.form['insertHI']
-            added = insertIntoHealthInsurance(coverage)
-            return render_template("addHealthInsurance.html", added = added)
+            #added = insertIntoHealthInsurance(coverage)
+            return render_template("addHealthInsurance.html", added = coverage)
         except:
             return render_template('error.html')
     else: 
