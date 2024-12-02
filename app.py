4e725e9cad1774f07d5bda5dcfa344e8b88af6c3
Pegistar5 <ec1253@georgetown.edu>
2024-12-02 11:53:42 -0500

    add menu partially implemented (url temporarily removed from addHealthInsurance for testing purposes)

--- app.py
+++ app.py
@@ -10,15 +10,21 @@ def home():
 
 @app.route("/add/", methods=["POST"])
 def add():
-    return render_template("query.html", query = "add")
+    try:
+        if request.form.get("addRow") == "Health Insurance":
+            return render_template("addHealthInsurance.html")
+        elif request.form.get("addRow") == "BenefitType":
+            return render_template("home.html")
+    except:
+        return render_template("error.html")
 
 @app.route("/update/", methods=["POST"])
 def update():
-    return render_template("query.html", query = "update")
+    return render_template("query.html", query = "update") #placeholder
 
 @app.route("/delete/", methods=["POST"])
 def delete():
-    return render_template("query.html", query = "delete")
+    return render_template("query.html", query = "delete") #placeholder
 
 @app.route("/query/")
 def queryPage():
@@ -28,3 +34,7 @@ def queryPage():
 def showall():
     table = request.form.get("show")
     return render_template("query.html", query = table)
+
+@app.route("/addHealthInsurance/", methods=['GET', 'POST'])
+def insertIntoHealthInsurance():
+    return render_template("addHealthInsurance.html")
