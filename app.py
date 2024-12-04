a2b76a4043f39c22d909cd00b6384f434031f393
Pegistar5 <ec1253@georgetown.edu>
2024-12-03 20:59:53 -0500

    Fixed q2

--- app.py
+++ app.py
@@ -997,7 +997,7 @@ def query2():
       con = sqlite3.connect("database.db")
       con.row_factory = sqlite3.Row
       cursor = con.cursor()
-      cursor.execute('SELECT DISTINCT EmployeeID FROM EmployeeInfo e, EmployeeAssignments d WHERE e.employeeID = d.employeeID and (d.EndDate IS NULL OR d.EndDate >= date()) and d.Department = ?', (dept,))
+      cursor.execute('SELECT DISTINCT e.EmployeeID FROM EmployeeInfo e, EmployeeAssignments d WHERE e.employeeID = d.employeeID and (d.EndDate IS NULL OR d.EndDate >= date()) and d.Department = ?', (dept,))
       info = cursor.fetchall()
       cursor.close()
     except Exception as e:
